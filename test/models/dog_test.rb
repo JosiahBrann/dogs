bc8bf316b63367694efbb3467537596f988fd886
josiahbrann <josiahbrann@gmail.com>
2019-03-23 17:28:05 -0500

    Add Dog model with tests

---
+++ test/models/dog_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class DogTest < ActiveSupport::TestCase
+  def setup
+    @valid_dog = dogs(:valid)
+    @invalid_dog = dogs(:invalid)
+  end
+
+  test 'valid dog' do
+    assert @valid_dog.valid?
+  end
+
+  test 'invalid without name' do
+    refute @invalid_dog.valid?, 'saved dog without a name'
+    assert_not_nil @invalid_dog.errors[:name], 'no validation error for name present'
+  end
+end
