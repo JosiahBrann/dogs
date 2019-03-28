22d64bdf589eccc902c3e2bd1cc23528a9f72176
Josiah Brann <jdbrann@ualr.edu>
2019-03-28 09:04:18 -0500

    Include Main Picture URL's in the API for Dogs
    This change includes the service_url from Active Storage for each Dog
    object in the serializer. This allows the api to include the primary
    dog picture url without the associated metadata.

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class DogSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :main_picture_url
+
+  def main_picture_url
+    object.main_picture.service_url if object.main_picture.attached?
+  end
 end
