09e01f417fa19f361069ca35f639a93fe5cb54da
Josiah Brann <jdbrann@ualr.edu>
2019-03-29 18:44:00 -0500

    Rescue error & return empty string if no dog main picture

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -4,6 +4,10 @@ class DogSerializer < ActiveModel::Serializer
   attributes :id, :name, :main_picture_url
 
   def main_picture_url
-    object.main_picture.service.send(:object_for, object.main_picture.key).public_url
+    if object.main_picture.attached?
+      return object.main_picture.service.send(:object_for, object.main_picture.key).public_url
+    end
+
+    ''
   end
 end
